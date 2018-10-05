ce5be06743ce0aa2044534cfa618525c1624c1d7
Andrew Chaa <andrew.yh.chaa@gmail.com>
2018-10-05 16:15:07 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import clipboard from 'clipboard-polyfill';
+import beautify from 'js-beautify';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
+
 export class AppComponent {
+
+  private _appService;
+  constructor(appService: AppService) {
+    this._appService = appService;
+  }
+
   title = 'app';
+  jsonString = 'Paste your JSON here';
+  pasteJson(): void {
+    console.log('t');
+    clipboard.readText().then(
+      txt => this.jsonString = beautify(txt, { indent_size: 4, space_in_empty_paren: true })
+    );
+  }
 }
